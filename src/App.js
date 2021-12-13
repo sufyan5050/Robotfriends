542f80b6dff5679d529b173498a6d7e7467157c0
sufyan5050 <skhelpp@gmail.com>
2021-12-13 08:21:54 +0500

    update robofriend app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import Cardlist from './Cardlist';
 import {Robot} from './Robot';
-import SearchBox from './SearchBox'
+import SearchBox from './SearchBox';
+import './App.css';
 
 class App extends Component {
     constructor(){
@@ -11,15 +12,19 @@ class App extends Component {
             searchfield:''
         }
     }
-    onsearchChange(event){
-         console.log(event);
+    onsearchChange = (event) =>{
+         this.setState({searchfield:event.target.value})
+         console.log(event.target.value);
     }
     render(){
+        const filterRobot = this.state.Robot.filter(Robot =>{
+            return Robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return(
             <div className = 'tc'>
-            <h1>Robofriends</h1>
+            <h1 className='f2'>Robofriends</h1>
             <SearchBox searchChange={this.onsearchChange}/>
-            <Cardlist Robot={this.state.Robot}/> 
+            <Cardlist Robot={filterRobot}/> 
             </div>  
         );
     }
